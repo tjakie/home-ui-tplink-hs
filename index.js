8d89cf9cfb2aeef06ac920dc54381821ed919d9f
tjakie <sjaak.hijmering@gmail.com>
2018-04-23 20:23:41 +0200

    connected to mainApi.js

--- index.js
+++ index.js
@@ -41,96 +41,8 @@ if (!processArguments.config.interval) {
 /**
   * DEFAULT API FUNCTION
   **/
-const http = require('http');
-const querystring = require('querystring');
-
-function requestApi (uri, method, data, cb) {
-	if (method === undefined) {
-		method = "GET"
-	}
-	
-	if (data === undefined) {
-		data = {};
-	}
-	
-	var req = http.request({
-		host: "::1",
-		port: 8123,
-		path: "/api/" + uri,
-		method: method,
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		}
-	}, (res) => {
-		var body = "";
-		
-		res.setEncoding('utf8');
-		res.on('data', (chunk) => {
-			body += chunk;
-		});
-		
-		res.on('end', () => {
-			if (cb) {
-				cb(null, JSON.parse(body));
-			}
-		});
-	});
-	
-	req.on('error', (e) => {
-		if (cb) {
-			cb({
-				"httperror": e.message
-			});
-		}
-	});
-
-	
-	req.write(querystring.stringify(data));
-	req.end();
-} 
-
-var net = require('net');
-var tcpSocket = false;
-
-deviceChangeEvents = [];
-
-function onDeviceChange(id, cb) {
-	if (tcpSocket === false) {
-		tcpSocket = new net.Socket();
-
-		tcpSocket.connect(8124, '127.0.0.1', function() {
-			
-		});
-		
-		tcpSocket.on('data', function(data) {
-			var json = false;
-			try {
-				json = JSON.parse(data);
-			}
-			catch (e) {
-				
-			}
-			
-			if (json.deviceValues) {
-				for (var i = 0; i < deviceChangeEvents.length; i ++) {
-					for (var id in json.deviceValues) {
-						if (deviceChangeEvents[i].id == id) {
-							deviceChangeEvents[i].cb(json.deviceValues[id]);
-							break;
-						}
-					}
-				}
-			}
-		});
-	}
-	
-	deviceChangeEvents.push({
-		id: id,
-		cb: cb
-	});
-}
-
-
+var homeUiApi = require("../../frontend/mainApi.js");
+ 
 
 /**
   * LOGIC
@@ -159,7 +71,7 @@ if (false) {
 
 
 
-requestApi("device", "POST", {
+homeUiApi.requestApi("device", "POST", {
 	name: processArguments.name,
 	type: "switch"
 }, function (err, id) { 
@@ -170,7 +82,7 @@ requestApi("device", "POST", {
 		throw "api returns strange result!";
 	}
 	else {
-		requestApi("deviceValue/" + id, "GET", {}, function (err, curVal) {
+		homeUiApi.requestApi("deviceValue/" + id, "GET", {}, function (err, curVal) {
 			var isInErrorState = false;
 			var goToState = null;
 			function nextStep () {
@@ -204,7 +116,7 @@ requestApi("device", "POST", {
 							if (curVal !== state) {
 								curVal = state;
 								
-								requestApi("deviceValue", "POST", {
+								homeUiApi.requestApi("deviceValue", "POST", {
 									id: id,
 									value: curVal? 100 : 0
 								}, function () {
@@ -221,7 +133,7 @@ requestApi("device", "POST", {
 
 			nextStep();
 			
-			onDeviceChange(id, function (deviceData) {
+			homeUiApi.onDeviceChange(id, function (deviceData) {
 				var newVal = deviceData.value === 100;
 				if (newVal !== curVal) {
 					goToState = newVal;
